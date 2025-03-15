495d2eca44fa8219f4685a33e95f444b340dd02f
Gib2005 <gibeonplaylist@gmail.com>
2025-03-15 15:24:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+ function showMessage() {
+    document.getElementById("message").innerText = "This isn't just code, this is my heart speaking. And it says... I miss you, Mikaela!\u2764\uFE0F" 
+ }
+
