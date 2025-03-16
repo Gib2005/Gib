5f9a4b491f6700b11ddc04d00371fbdf21b9247f
Gib <gibeonplaylist@gmail.com>
2025-03-16 22:30:24 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
  function showMessage() {
     let messageElement = document.getElementById("message");
-    messageElement.innerText = "DI KA CRUSH NG CRUSH MO! \n 💀";
+    messageElement.innerText = "DI KA CRUSH NG CRUSH MO! \n\n💀";
     messageElement.classList.remove("goldenrod-text");
     messageElement.classList.add("violet-text");
    
